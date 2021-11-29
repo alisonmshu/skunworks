2adfa5719f6fe5b2d8c7a997f16cc4102251a75c
alisonmshu <ashu@ironflytechnologies.com>
2021-11-29 22:07:15 +0800

    add scala project

---
+++ ScalaProject/src/main/scala/com/alison/scala/Demo.scala
@@ -0,0 +1,10 @@
+package com.alison.scala
+
+// Scala 2
+object HelloScala2 {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala 2")
+  }
+}
+// Scala 3
+@main def HelloScala3 = println("Hello Scala 3")
