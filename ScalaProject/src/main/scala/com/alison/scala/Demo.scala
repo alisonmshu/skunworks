504c79ff6534e7a5e7786c7568e8a09b7eced25c
Alison Shu <ashu@ironflytechnologies.com>
2021-12-03 17:51:49 +0800

    fix bubble sort

--- ScalaProject/src/main/scala/com/alison/scala/Demo.scala
+++ ScalaProject/src/main/scala/com/alison/scala/Demo.scala
@@ -1,10 +1,15 @@
 package com.alison.scala
 
 // Scala 3
+
+//def Swap (a: Int, b: Int) = // Method parameters are immutable in Scala
+//  val tmp: Int = a
+//  a = b
+//  b = tmp
+
 def BubbleSort (items: Array[Int]) =
-  val len: Int = items.length
-  println(len)
-  for (i <- 0 to (len - 1)) {
+  val len: Int = items.length - 1
+  for (i <- 0 to len) {
     for(j <- 0 to (len - i - 1)) {
       if items(j) > items(j + 1) then
         var tmp: Int = items(j)
@@ -12,7 +17,6 @@ def BubbleSort (items: Array[Int]) =
         items(j + 1) = tmp
     }
   }
-
 
 def BinarySearch(items: Array[Int], left: Int, right: Int, searchItem: Int): Int =
   if right.>=(left) then
@@ -26,11 +30,13 @@ def BinarySearch(items: Array[Int], left: Int, right: Int, searchItem: Int): Int
   else
     return -1
 
-@main def Run =
+@main def main =
   val arr = Array(2, 3, 4, 10, 40, 1, 7)
   BubbleSort(arr)
+  println("Array is " + arr.toList)
   val len = arr.length
   val x = 7
+  println("Search = " + x)
   val result = BinarySearch(arr, 0, len - 1, x)
   if result.equals(-1) then
     println("404 Not Found.")
