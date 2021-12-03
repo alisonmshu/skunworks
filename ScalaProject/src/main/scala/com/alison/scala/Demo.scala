a67cb770968df22d6da5041a0c4fc796763a4327
Alison Shu <ashu@ironflytechnologies.com>
2021-12-03 16:16:57 +0800

    try to build binary search on scala

--- ScalaProject/src/main/scala/com/alison/scala/Demo.scala
+++ ScalaProject/src/main/scala/com/alison/scala/Demo.scala
@@ -1,10 +1,15 @@
 package com.alison.scala
 
-// Scala 2
-object HelloScala2 {
-  def main(args: Array[String]): Unit = {
-    println("Hello Scala 2")
-  }
-}
 // Scala 3
-@main def HelloScala3 = println("Hello Scala 3")
+val x = List(2, 7, 4, 8, 23, 44, 11)
+
+@main def BinarySearch(searchItem: Int, items: Array[Int], left: Int, right: Int) =
+  if right >= 1 then
+    var mid: Int = 1 + (right - left)/2
+    if items(mid).eq(x) then
+      return mid
+    else if items(mid) > x then
+
+
+
+
